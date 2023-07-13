b70a03a3536da7b67a5c4f097913d3733b07ffdf
jerry-the-kid <jerrycarry79@gmail.com>
2023-07-13 22:27:28 +0700

    Day 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './layout/ui/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+
   ],
   imports: [
     BrowserModule,
